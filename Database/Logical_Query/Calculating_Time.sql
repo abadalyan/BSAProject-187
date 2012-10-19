17063b45bff5c96a5439ac8830768cafe14c428c
Albert B. <mr.abadalyan@gmail.com>
2012-10-19 08:24:25 -0700

    Updated Calculating_Time.SQL

--- Database/Logical_Query/Calculating_Time.sql
+++ Database/Logical_Query/Calculating_Time.sql
@@ -7,26 +7,30 @@
 --AS
 Declare @CaseEmpID int
 SET @CaseEmpID = 4
+
 /* Declare variables to be used for calculations. */
-DECLARE @sql1 nvarchar(100), @sql2 nvarchar(100), @date1 nvarchar(60), @date2 nvarchar(60), @calc decimal(7,2)
+DECLARE @sql1 nvarchar(255), @sql2 nvarchar(255), @date1 nvarchar(60), @date2 nvarchar(60), @calc decimal(7,2)
 DECLARE @retval1 int, @retval2 int, @ParmDef1 nvarchar(255), @ParmDef2 nvarchar(255)
 
 /* Prepare query to capture output for @date1 (START_TIME) from sp_executesql. */
 SET @sql1 = 'select @date1OUT = START_TIME from [dbo].[case_employee] where case_emp_ID =' +Convert(nvarchar,@CaseEmpID)+''
 SET @ParmDef1 = N'@date1OUT datetime OUTPUT'
 
-/* Execute @sql1 and capture the output into @date1  (START_TIME) for later calculaitons. */ 
+/* Execute @sql1 and capture the output into @date1  (START_TIME) for later calculations. */ 
 EXEC sp_executesql @sql1, @ParmDef1, @date1OUT=@date1 OUTPUT;
 
 /* Prepare query to capture output for @date2 (END_TIME) from sp_executesql. */
 SET @sql2 = 'select @date2OUT = END_TIME from [dbo].[case_employee] where case_emp_ID =' +Convert(nvarchar,@CaseEmpID)+''
 SET @ParmDef2 = N'@date2OUT datetime OUTPUT'
 
-/* Execute @sql1 and capture the output into @date2 (END_TIME) for later calculaitons. */
+/* Execute @sql1 and capture the output into @date2 (END_TIME) for later calculations. */
 EXEC sp_executesql @sql2, @ParmDef2, @date2OUT=@date2 OUTPUT;
 
+/* Calculate and store the difference between the START_TME and END_TIME. 
+Also, divide the minutes calculated by 60 and convert to float for preserve decimal values.*/
 set @calc = Convert(Float,DATEDIFF(minute, @date1, @date2)/60.0)
 
+/* Append the calculated difference to the record with matching case_emp_ID.*/
 update [dbo].[case_employee] 
 set CALC_TIME = @calc
 where case_emp_ID = @CaseEmpID

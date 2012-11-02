82c7206718dfcf00f2078ae971a9a2e8ee9ff9c1
Albert B. <mr.abadalyan@gmail.com>
2012-11-01 21:03:32 -0700

    Report and Updated Calculating_Time

--- Database/Logical_Query/Calculating_Time.sql
+++ Database/Logical_Query/Calculating_Time.sql
@@ -1,16 +1,40 @@
+USE [LawFirmDRM2]
+GO
 
-/*Changelog*/
-/*10/18/12: Currenly can calculate the time difference for a single Case_Emp_ID that is provided in the script.
-			Ultimately, this script will calculate the CALC_TIME for each record in the dbo.case_employee table*/
+/****** Object:  StoredProcedure [dbo].[sp_CalcTime]    Script Date: 11/1/2012 7:25:41 PM ******/
+SET ANSI_NULLS ON
+GO
 
---CREATE PROCEDURE [dbo].[sp_CalcDate] @CaseID int
---AS
-Declare @CaseEmpID int
-SET @CaseEmpID = 4
+SET QUOTED_IDENTIFIER ON
+GO
 
+
+ALTER procedure [dbo].[sp_CalcTime]
+AS
+/*
+Changelog:
+Date      Comment
+------------------------------------------------------------------------------------------------------------
+10/18/12: Currenly can calculate the time difference for a single Case_Emp_ID that is provided in the script.
+			Ultimately, this script will calculate the CALC_TIME for each record in the dbo.case_employee table.
+10/28/12: Procedure will now perform the date-time calculation for any existing record with a CASE_EMP_ID. Also
+			will update the CALC_TIME for each record unless the calculated time is < .5.
+11/1/12: Updated the ELSEIF statement to update the CALC_TIME to NULL.
+------------------------------------------------------------------------------------------------------------
+*/
 /* Declare variables to be used for calculations. */
 DECLARE @sql1 nvarchar(255), @sql2 nvarchar(255), @date1 nvarchar(60), @date2 nvarchar(60), @calc decimal(7,2)
 DECLARE @retval1 int, @retval2 int, @ParmDef1 nvarchar(255), @ParmDef2 nvarchar(255)
+DECLARE @CaseEmpID int
+
+/* Declare curor. */
+DECLARE CaseEmpID CURSOR FOR
+SELECT case_emp_id FROM [dbo].[case_employee];
+OPEN CaseEmpID
+FETCH NEXT FROM CaseEmpID 
+INTO @CaseEmpID
+WHILE @@FETCH_STATUS = 0
+BEGIN
 
 /* Prepare query to capture output for @date1 (START_TIME) from sp_executesql. */
 SET @sql1 = 'select @date1OUT = START_TIME from [dbo].[case_employee] where case_emp_ID =' +Convert(nvarchar,@CaseEmpID)+''
@@ -30,9 +54,25 @@ EXEC sp_executesql @sql2, @ParmDef2, @date2OUT=@date2 OUTPUT;
 Also, divide the minutes calculated by 60 and convert to float for preserve decimal values.*/
 set @calc = Convert(Float,DATEDIFF(minute, @date1, @date2)/60.0)
 
-/* Append the calculated difference to the record with matching case_emp_ID.*/
+/* 
+Append the calculated difference to the record with matching case_emp_ID.
+Update the CALC_TIME if @calc is greater, or equal to, .5. Otherwise, notify user.
+*/
+IF @calc >= .5
+BEGIN
 update [dbo].[case_employee] 
 set CALC_TIME = @calc
 where case_emp_ID = @CaseEmpID
+END 
+ELSE
+update dbo.case_employee
+set CALC_TIME = NULL
+where case_emp_ID = @CaseEmpId
+--PRINT 'The following Case_Emp_ID was not updated due to a negative value (END_TIME not present):' + Convert(varchar(10),@CaseEmpID)
+FETCH NEXT FROM CaseEmpID INTO @CaseEmpID
+END
+CLOSE CaseEmpID
+DEALLOCATE CaseEmpID
+GO
 
---Select @calc
+
